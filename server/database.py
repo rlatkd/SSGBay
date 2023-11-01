ac3d4864692b44edd76221cee1ad43e769883529
rlatkdgns <rlatkdgns042@naver.com>
2023-11-01 20:52:01 +0900

    new file:   client/.gitignore 	new file:   client/Dockerfile-react 	new file:   client/README.md 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/favicon.ico 	new file:   client/public/index.html 	new file:   client/public/logo192.png 	new file:   client/public/logo512.png 	new file:   client/public/manifest.json 	new file:   client/public/robots.txt 	new file:   client/src/App.css 	new file:   client/src/App.js 	new file:   client/src/App.test.js 	new file:   client/src/components/Card.js 	new file:   client/src/components/Card.module.css 	new file:   client/src/components/CreatePage.module.css 	new file:   client/src/components/DetailPage.module.css 	new file:   client/src/components/Footer.js 	new file:   client/src/components/Footer.module.css 	new file:   client/src/components/Header.js 	new file:   client/src/components/Header.module.css 	new file:   client/src/components/LoginPage.module.css 	new file:   client/src/components/MainPage.module.css 	new file:   client/src/components/MyPage.module.css 	new file:   client/src/index.css 	new file:   client/src/index.js 	new file:   client/src/logo.svg 	new file:   client/src/pages/CreatePage.js 	new file:   client/src/pages/DetailPage.js 	new file:   client/src/pages/LoginPage.js 	new file:   client/src/pages/MainPage.js 	new file:   client/src/pages/MyPage.js 	new file:   client/src/pages/SignupPage.js 	new file:   client/src/reportWebVitals.js 	new file:   client/src/setupTests.js 	new file:   k8s/flask-deployment.yaml 	new file:   k8s/mysql-deployment.yaml 	new file:   k8s/nfs-deployment-service.yaml 	new file:   k8s/nfs-persistentvolume.yaml 	new file:   k8s/nfs-persistentvolumeclaim-pod.yaml 	new file:   k8s/react-deployment.yaml 	new file:   server/Dockerfile-flask 	new file:   server/Dockerfile-mysql 	new file:   server/__pycache__/database.cpython-312.pyc 	new file:   server/app.py 	new file:   server/crontabFile 	new file:   server/database.py 	new file:   server/historyUpdate.py 	new file:   server/init.sql 	new file:   server/node_modules/.package-lock.json 	new file:   server/node_modules/flask-cors/package.json 	new file:   server/package-lock.json 	new file:   server/package.json 	new file:   server/requirements.txt 	new file:   server/resources/image.jpeg 	new file:   server/resources/me.png 	new file:   "server/resources/\352\271\200\354\203\201\355\233\210.JPEG" 	new file:   "server/resources/\354\232\260\354\230\201\353\257\270.JPEG" 	new file:   "server/resources/\354\272\241\354\262\230.JPG"

---
+++ server/database.py
@@ -0,0 +1,199 @@
+from pymysql import connect
+from datetime import datetime
+import shutil
+from flask import Flask, json, jsonify
+from os import path
+import pymysql
+
+
+connectionString = {
+    'host': '172.17.14.241', # mysql-service의 IP
+    'port': 3306,
+    'database': 'auction',
+    'user': 'user1',
+    'password': '1234',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+
+def idCheck(user_id, pwd):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "SELECT * FROM user " + "where id = %s and password = %s;"
+            cursor.execute(sql, [user_id, pwd])
+            result = cursor.fetchall()
+            
+            return result
+        
+    except Exception as e:
+        print(e)
+
+
+def getMyItem(user_id):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "SELECT * FROM item where user_id = %s;"
+            cursor.execute(sql, [user_id])
+            result = cursor.fetchall()
+            
+            return result
+        
+    except Exception as e:
+        print(e)        
+
+
+def getItems(sort, keyword):
+    try:
+        query = "SELECT * FROM item"
+  
+        # Conditionally filter by keyword
+        if keyword:
+            query += f" WHERE name LIKE '%{keyword}%' OR content LIKE '%{keyword}%'"
+                
+        if sort == "priceDown":
+            query += " ORDER BY price DESC"
+        elif sort == "priceUp":
+            query += " ORDER BY price ASC"
+        else:
+            query += " ORDER BY startTime DESC"
+            
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            cursor.execute(query)
+            itemInfo = cursor.fetchall()
+            cursor.close()
+        
+        if not itemInfo:
+            
+            return [], 200, { 'Content-Type': 'application/json'}
+
+        return itemInfo, 200, { 'Content-Type': 'application/json'}
+
+    except Exception as e:
+        print(e)
+
+
+# 메인페이지에서 1번상품을 들어가면 1번상품에 해당하는 내용이 나와야하기떄문에 id인자와 쿼리문 살짝 수정
+def getItemDetails(id):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "select * from item where id = %s"
+            cursor.execute(sql, (id, ))
+            itemDetails = cursor.fetchone()
+            cursor.close()
+            
+        return itemDetails, 200, { 'Content-Type': 'application/json'}
+
+    except Exception as e:
+        print(e)
+        
+        
+def addUserInfo(userId, userPwd, userNickname, userPhone):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = f"""INSERT INTO user (id, password, nickname, phone) VALUES("{userId}","{userPwd}","{userNickname}","{userPhone}")"""
+            print(cursor.execute(sql))
+            userInfo = cursor.fetchall()
+            con.commit()
+        
+        return userInfo, 200, { 'Content-Type': 'application/json'}    
+            
+    except Exception as e:
+        print(e)
+
+
+def getMyItem(user_id):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "SELECT * FROM item where user_id = %s;"
+            cursor.execute(sql, [user_id])
+            result = cursor.fetchall()
+            
+            return result
+        
+    except Exception as e:
+        print(e) 
+                
+
+def getBuyItem(user_id):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "SELECT item.* FROM item INNER JOIN history ON item.id = history.item_id WHERE history.user_id = %s;"
+            cursor.execute(sql, [user_id])
+            result = cursor.fetchall()
+            print(result)
+            
+            return result
+        
+    except Exception as e:
+        print(e)        
+
+        
+def addItemInfo(itemName, itemContent, itemPrice, itemImage, endTime, userId):
+    
+    with connect(**connectionString) as con:
+            cursor = con.cursor()
+            now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            # SQL 쿼리 수정: 플레이스홀더를 사용하여 SQL 인젝션을 방지
+            sql = "INSERT INTO item (name, content, price, image, endTime, startTime, user_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (itemName, itemContent, itemPrice, itemImage, endTime, now, userId))
+            con.commit()
+
+    return "경매물품 등록 성공", 200
+ 
+
+# database.py
+def getItemDetails(id):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "select * from item where id = %s"
+            cursor.execute(sql, (id, ))
+            itemDetails = cursor.fetchone()
+            cursor.close()
+            
+        return itemDetails, 200, { 'Content-Type': 'application/json'}
+
+    except Exception as e:
+        print(e)
+        
+def updatePrice(id, price, new_price):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            sql = "UPDATE item SET price = %s WHERE id = %s"
+            cursor.execute(sql, (new_price,id))
+            con.commit()
+            cursor.close()
+            
+            return {"message": "입찰되었습니다."}, 200
+
+    except Exception as e:
+        print(e)
+        return {"message": "가격 업데이트에 실패했습니다."}, 500
+
+
+def insertPrehistory(itemId, userId, endTime):
+    try:
+        with connect(**connectionString) as con:
+            cursor = con.cursor()
+            formatted_date = datetime.strptime(endTime[:-4], '%a, %d %b %Y %H:%M:%S')
+            formatted_date_for_mysql = formatted_date.strftime('%Y-%m-%d %H:%M:%S')
+            sql = "INSERT INTO prehistory (item_id, user_id, endTime) VALUES (%s, %s, %s)"
+            cursor.execute(sql, (itemId, userId, formatted_date_for_mysql))
+            con.commit()
+            cursor.close()
+            
+            return {"message": "거래 저장"}, 200
+
+    except Exception as e:
+        print(e)
+        
+        return {"message": "거래 실패"}, 500
